92e09701c530c324bd101c473ef96f27af273439
Ryan <ryanwbachman@gmail.com>
2017-09-16 01:16:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(n)
   n = n.to_i
   if n % 15 == 0
     return "FizzBuzz"
-  else if n % 3 == 0
+  elsif n % 3 == 0
     return "Fizz"
-  else if n % 5 == 0
+  elsif n % 5 == 0
     return "Buzz"
   else
     return ""
